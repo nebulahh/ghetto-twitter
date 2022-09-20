9643ffb874adea627dcba9cd73736dad1c8ac6d0
Nkechi-Christabel <udenkwornkechi@gmail.com>
2022-09-20 21:57:19 +0100

    Added comments and created dynamic delete function

--- routes/comments.js
+++ routes/comments.js
@@ -4,11 +4,11 @@ const commentsController = require("../controllers/comments");
 const { ensureAuth, ensureGuest } = require("../middleware/auth"); //This is so the 'ensureAuth' method can be called the the get('/:id') route
 
 //Comments Routes - simplified for now
-router.post("/createComment/:id", commentsController.createComment); 
+router.post("/createComment/:id", commentsController.createComment);
 
 //adding comment likes
-// router.put("/likeComment/:id", commentsController.likeComment);
+router.put("/likeComment/:id", commentsController.likeComment);
 
-// router.delete("/deleteComment/:id", commentsController.deleteComment);
+router.delete("/deleteComment/:id", commentsController.deleteComment);
 
 module.exports = router;
